09a54b76c7bc0c5f86459058fac76478f3f3068a
阳光彩虹小白马 <1373617365@qq.com>
2020-10-27 17:15:26 +0800

    add

---
+++ vue_webpack/src/todo/header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="main-header">
+        <h1>JTodo</h1>
+    </header>
+</template>
+<style lang="less">
+h1{
+    color: rgba(175,47,47,0.4);
+    text-align: center;
+    font-size: 100px;
+    margin: 20px;
+}
+</style>
